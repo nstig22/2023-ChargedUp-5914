3ec1bf600c91c420ad9da8946e5d1f762e007bf8
5914 Programming <60047732+nstig22@users.noreply.github.com>
2023-01-30 20:27:59 -0600

    Update Constants.java

--- FullSwerveDrive/src/main/java/frc/robot/Constants.java
+++ FullSwerveDrive/src/main/java/frc/robot/Constants.java
@@ -6,12 +6,12 @@ public final class Constants {
   // FIXME-These values need to reflect the actual specs of the swerve modules
   public static final class ModuleConstants {
     public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
-    public static final double kDriveMotorGearRatio = 999.999;
+    public static final double kDriveMotorGearRatio = 0.103;
     public static final double kTurnMotorGearRatio = 999.999;
     public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
     public static final double kTurningEncoderRot2Rad = kTurnMotorGearRatio * 2 * Math.PI;
-    public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 999.999;
-    public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 999.999;
+    public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+    public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
     public static final double kPTurning = 999.999;
   }
 }
